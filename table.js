29bf06b5801f5d9bea842ac0f038aab50de67b77
Maruthupandian M <maruthupandian72@gmail.com>
2021-10-19 09:08:16 +0530

    added validation for input fields

--- table.js
+++ table.js
@@ -4,6 +4,17 @@ var empid = document.getElementById("edit-empid");
     var email = document.getElementById("edit-email");
     var table = document.getElementById("mytable"), rIndex;
     var trs=document.getElementsByTagName("tr");
+
+ function deleteRow(){
+                                    
+                                
+                                deleteindex.indexvalue.parentNode.removeChild(deleteindex.indexvalue);
+                                
+                                   
+                                }
+
+
+
 let obj={
     "indexvalue": 0,
     "empid":'',
@@ -11,8 +22,22 @@ let obj={
     "dob":"",
     "email":"",
 };
+let deleteindex={
+    "indexvalue":0,
+};
 
+function alertWindow(indexvalue){
+var row = indexvalue.parentNode.parentNode;
+deleteindex.indexvalue=row;
 
+var r=confirm("Are you sure you want to delete this Row?");
+if (r==true) {
+  deleteRow();
+} else {
+  
+}
+
+}
  
 
 
@@ -95,12 +120,7 @@ empid.value = obj.empid;
 
 function save(){
    
-    console.log(obj.indexvalue);
-    document.getElementById("editable").style.display = "none";
-    document.getElementById("mytable").style.display = "table";
-    document.getElementById("search").style.display = "inline";
-    document.getElementById("add-btn").style.display = "inline";
-    document.getElementById("add").style.display="none";
+    
 
      var empid = document.getElementById("edit-empid");
      var empname = document.getElementById("edit-name");
@@ -120,5 +140,30 @@ var dateString = (d <= 9 ? '0' + d : d) + '-' + (m <= 9 ? '0' + m : m) + '-' + y
     table.rows[obj.indexvalue].cells[1].innerHTML=empname.value;
 table.rows[obj.indexvalue].cells[2].innerHTML=dateString;
 table.rows[obj.indexvalue].cells[3].innerHTML=email.value;
+
+if (empid.value == "" || empname.value=="" || dob.value==""||email.value=="" ) {
+    alert("All fields are required!");
+    
+  }
+  else{
+console.log(obj.indexvalue);
+    document.getElementById("editable").style.display = "none";
+    document.getElementById("mytable").style.display = "table";
+    document.getElementById("search").style.display = "inline";
+    document.getElementById("add-btn").style.display = "inline";
+    document.getElementById("add").style.display="none";
+  }
+
+
+
+
+
+}
+function cancel(){
+    document.getElementById("editable").style.display = "none";
+    document.getElementById("mytable").style.display = "table";
+    document.getElementById("search").style.display = "inline";
+    document.getElementById("add-btn").style.display = "inline";
+    document.getElementById("add").style.display="none";
 }
 
